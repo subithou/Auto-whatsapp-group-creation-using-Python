5c27519f9e6de8e4372e96a106812c0a91fa4f07
Subith O U <57960146+subithou@users.noreply.github.com>
2022-02-16 12:58:45 +0530

    Python file for create whatsapp group

--- auto_whatsapp_group.py
+++ auto_whatsapp_group.py
@@ -1,23 +1,22 @@
 from time import time
 from pyautogui import hotkey, press, typewrite 
 
-# move to whatsapp web 
+# move to whatsapp desktop version
 hotkey('alt', 'tab')
 
-# short key for open create new group in whatsapp web
-# hotkey('ctrl', 'shift', 'n' )
-hotkey('ctrl', 'alt','shift', 'n' )
+# short key for open create new group in whatsapp desktop version
+hotkey('ctrl', 'shift', 'n' )
+
 # Specify how many groups you want to create
 no_of_group = 1
 
-
 for j in range(0,no_of_group*2): 
     for i in range(0,1):
-        typewrite("unnikkuttan")  # Specify the contact name as per your phone with in double quote
+        typewrite("name of the contact")  # Specify the contact name as per your phone with in double quote
         press('enter')
     
     press('enter')
-    typewrite("GRP4") # Specify group name with in double quote
+    typewrite("name of the group") # Specify group name with in double quote
     press('enter')  # In this step we successfully complete the creation of a group
     hotkey('ctrl', 'shift', 'n' ) # short key for open create new group 
 
